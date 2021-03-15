8de1f55d9ced5e60f539d2437877f397fca423a4
pbgm5 <1priyabegum@gmail.com>
2021-03-15 16:47:10 -0400

    finsihed python versions of saytwice and temperature

---
+++ temperature.py
@@ -0,0 +1,9 @@
+tempincelsius = input("Please enter a temperature in celsuis...")
+# print(tempincelsius)
+# print((tempincelsius))
+
+tempinfahrenheit = float(tempincelsius * 1.8 + 32
+print(tempincelsius)
+print((tempincelsius))
+
+# print(tempincelsius + " degrees celsius is " + "tempinfahrenheit.")
